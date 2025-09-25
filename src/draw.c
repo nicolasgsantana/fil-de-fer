3d9ab2470329d5a45e01bc66e1577a9206fc8c9a
nicolasgsantana <nicolas07san@gmail.com>
2025-09-25 16:24:47 -0300

    add new calc_x and calc_y formulas

--- src/draw.c
+++ src/draw.c
@@ -6,30 +6,20 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 14:55:56 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/25 15:58:37 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/09/25 16:09:05 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	calc_x(int x, int y, int z)
+int	calc_x(int x, int y)
 {
-	int	x_pos;
-	int	angle;
-
-	angle = 45;
-	x_pos = (x * 10)  * cos(angle) + (y * 10) * cos(angle + 2) + (z * 5) * cos(angle - 2);
-	return (x_pos);
+	return ((x - y) * sqrt(3) / 2);
 }
 
 int	calc_y(int x, int y, int z)
 {
-	int	y_pos;
-	int	angle;
-
-	angle = 45;
-	y_pos = (x * 10)  * sin(angle) + (y * 10)  * sin(angle + 2) + (z * 5) * sin(angle - 2);
-	return (y_pos);
+	return ((x + y) / 2 - z);
 }
 
 void	draw_points(mlx_image_t *img, t_list *map)
@@ -47,8 +37,8 @@ void	draw_points(mlx_image_t *img, t_list *map)
 		x = 0;
 		while (row[x])
 		{
-			mlx_put_pixel(img, calc_x(x, y, row[x][0]) + 200, calc_y(x, y, row[x][0]) + 200, row[x][1]);
-			ft_printf("X: %d, Y: %d\n", calc_x(x, y, row[x][0]), calc_y(x, y, row[x][0]));
+			mlx_put_pixel(img, calc_x(x, y) + 200, calc_y(x, y, row[x][0]) + 200, row[x][1]);
+			ft_printf("X: %d, Y: %d\n", calc_x(x, y), calc_y(x, y, row[x][0]));
 			x++;
 		}
 		y++;
