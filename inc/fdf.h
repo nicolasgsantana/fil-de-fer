8b2fd3630837de6c9630e9a71ee2e847ad3b0dbb
nicolasgsantana <nicolas07san@gmail.com>
2025-11-06 13:38:32 -0300

    add t_point type

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 14:46:40 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/11/06 13:29:00 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/11/06 13:38:05 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@
 # define HEIGHT 720
 # define DEFAULT_COLOR 0xFFFFFFFF
 # define SPACE 10
+
+typedef struct s_point
+{
+	int				x;
+	int				y;
+	int				z;
+	unsigned int	color;
+}					t_point;
 
 void	free_char_array(char **array);
 size_t	ft_arrlen(char **array);
