829f88994bd97e39097a50764d39d71f8b71c702
nicolasgsantana <nicolas07san@gmail.com>
2025-11-12 16:22:32 -0300

    add bresenham line algorithm

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 14:46:40 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/11/12 10:35:05 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/11/12 16:06:52 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ typedef struct s_point
 
 unsigned int	ft_htoui(char *hex);
 t_point			*parse(char *file);
-void	draw_points(mlx_image_t *img, t_point *points);
+void			draw_points(mlx_image_t *img, t_point *points, int size);
+void 			plot_line(mlx_image_t *img, t_point p0, t_point p1);
 
 // DEBUG ONLY REMOVE LATER
 void	print_map(t_point *points, char *file);
--- src/draw.c
+++ src/draw.c
@@ -6,42 +6,46 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 14:55:56 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/09/25 16:09:05 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/11/12 16:19:34 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	calc_x(int x, int y)
+void	plot_line(mlx_image_t *img, t_point p0, t_point p1)
 {
-	return ((x - y) * sqrt(3) / 2);
-}
+	int	dx;
+	int	dy;
+	int	d;
+	int	y;
+	int	x;
 
-int	calc_y(int x, int y, int z)
-{
-	return ((x + y) / 2 - z);
-}
-
-void	draw_points(mlx_image_t *img, t_list *map)
-{
-	t_list	*node;
-	int		**row;
-	int		x;
-	int		y;
-
-	node = map;
-	y = 0;
-	while (node)
+	dx = p1.x - p0.x;
+	dy = p1.y - p0.y;
+	d = 2 * dy - dx;
+	y = p0.y;
+	x = p0.x;
+	while (x <= p1.x)
 	{
-		row = (int **)node->content;
-		x = 0;
-		while (row[x])
+		mlx_put_pixel(img, x, y, 0xFFFFFFFF);
+		if (d > 0)
 		{
-			mlx_put_pixel(img, calc_x(x, y) + 200, calc_y(x, y, row[x][0]) + 200, row[x][1]);
-			ft_printf("X: %d, Y: %d\n", calc_x(x, y), calc_y(x, y, row[x][0]));
-			x++;
+			y++;
+			d = d - 2 * dx;
 		}
-		y++;
-		node = node->next;
+		d = d + 2 * dy;
+		x++;
+	}
+}
+
+void	draw_points(mlx_image_t *img, t_point *points, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		mlx_put_pixel(img, points[i].x * 30, points[i].y * 30, points[i].color);
+		i++;
 	}
 }
